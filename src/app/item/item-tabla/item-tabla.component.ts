dfdd5d96f01f36ae58850a5931e1cba6b6b72bc0
vargasjuanj <juanjovargas443377@gmail.com>
2021-10-01 14:00:44 -0300

    Crud ok, nuevas formas de ejecutar las acciones

--- src/app/item/item-tabla/item-tabla.component.ts
+++ src/app/item/item-tabla/item-tabla.component.ts
@@ -11,6 +11,10 @@ export class ItemTablaComponent implements OnInit {
 
 
   items: Item[] = []
+  createItem: Item = {name:''}
+  updateItem: Item = {name:''}
+  updateCheked = false
+  addItem = false
 
   constructor(private itemService: ItemService) { }
 
@@ -21,6 +25,14 @@ export class ItemTablaComponent implements OnInit {
     // Me engancho al observable readItem del itemService, y cada vez que se observable haga un next, voy a recibir los datos, y se va a ejecutar el alert.
     // La forma de que este observable genere un dato, es primero activando el metodo read(id) de abajo, entonces va a ir a buscar ese item, y en el iTemservice va a realizar un next(), y el dato va a venir a este componente
     this.itemService.getReadItem().subscribe(item => alert(item.id + ' ' + item.name + ' ' + item.description))
+
+
+    this.itemService.getUpdateItem().subscribe(item=> {
+      this.updateCheked = true
+      this.updateItem= item
+
+    }
+      )
   }
 
   delete(id:number){
@@ -33,5 +45,31 @@ export class ItemTablaComponent implements OnInit {
 
   update(item:Item){
     this.itemService.update(item)
+  }
+
+  prepareUpdate(id:number){
+    this.itemService.prepareUpdate(id)
+  }
+
+  save(){
+    this.updateCheked = false
+    this.itemService.update(this.updateItem)
+  }
+
+  cancel(){
+    this.updateCheked = false
+  }
+
+  add(){
+    this.addItem = true
+    this.createItem = {name:''}
+  }
+  cancelAdd(){
+    this.addItem = false
+  }
+
+  create (){
+    this.addItem = false
+    this.itemService.create(this.createItem)
   }
 }
